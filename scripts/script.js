15b3eecdb95f2eb23c1cb49b5fc3008f04dab090
Elena Parchevskaya <eparchevskaya@tut.by>
2020-04-14 17:13:00 +0300

    feat: полностью сверстать и сделать логику блока popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,40 @@
 let root = document.querySelector('.root');
-
 let content = root.querySelector('.content');
 let profile = content.querySelector('.profile');
 let editButton = profile.querySelector('.edit-button');
+let editForm = root.querySelector('.popup__edit');
+let popup = root.querySelector('.popup');
+let titleProfile = profile.querySelector('.profile__title');
+let subtitleProfile = profile.querySelector('.profile__subtitle');
+let nameInput = editForm.querySelector('.popup__item_name');
+let jobInput = editForm.querySelector('.popup__item_about');
+
 
 function formOpen() {
-  let popup = root.querySelector('.popup');
   popup.classList.add('popup_opened');
+
+  nameInput.value = titleProfile.textContent;
+  jobInput.value = subtitleProfile.textContent;
 
 }
 
 editButton.addEventListener('click', formOpen);
+
+let closeButton = root.querySelector('.popup__button_close');
+
+function formClose() {
+  popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', formClose);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  titleProfile.textContent = nameInput.value;
+  subtitleProfile.textContent = jobInput.value;
+
+  formClose();
+}
+
+editForm.addEventListener('submit', formSubmitHandler);
