4340c74592609685ab6af759df8715362c16016f
Elena Parchevskaya <eparchevskaya@tut.by>
2020-04-29 19:14:50 +0300

    refactor: добавление верстки попапа для кнопки добавить и логика открытия

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,9 @@ const editButton = profile.querySelector('.edit-button');
 const editForm = root.querySelector('.popup__edit');
 const popup = root.querySelector('.popup');
 const closeButton = root.querySelector('.popup__button_close');
+const addButton = root.querySelector('.add-button');
+const addPopup = root.querySelector('.add-popup');
+
 
 
 function formCloseOpen () {
@@ -17,16 +20,19 @@ function formCloseOpen () {
   nameInput.value = titleProfile.textContent;
   jobInput.value = subtitleProfile.textContent;
 
-  if(popup.classList.contains('popup_opened') !== false) {
-    popup.classList.toggle('popup_opened');
-  }
-  else {
-    popup.classList.toggle('popup_opened');
-  }
+  popup.classList.toggle('popup_opened');
+}
+
+function formAdd () {
+  let namePlace = editForm.querySelector('.popup__item_name-place');
+  let linkPlace = editForm.querySelector('.popup__item_link-place');
+  addPopup.classList.toggle('popup_opened');
 }
 
 editButton.addEventListener('click', formCloseOpen);
 closeButton.addEventListener('click', formCloseOpen);
+addButton.addEventListener('click', formAdd);
+
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
